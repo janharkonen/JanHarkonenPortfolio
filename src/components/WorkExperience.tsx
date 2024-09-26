7549ecd3f468aeb0bf084d5e0757442c40c2748f
Jan Härkönen <jan.harkonen@gmail.com>
2024-09-26 16:41:24 +0300

    Lisätty kokemuksia

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -13,8 +13,11 @@ const experiences = [
     tools: [
       { name: "Delphi", logo: "https://cdn.simpleicons.org/delphi" },
       { name: "Microsoft SQL Server", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7TIOHIVieZJ564CaI1B8LQAAAA%26pid%3DApi&f=1&ipt=aed1fbd637221218c13f8b4fc98bf59ba08fb142938dfdc707d75acdffba2f78&ipo=images" },
-      { name: "Git", logo: "https://cdn.simpleicons.org/git" },
       { name: "Sourcetree", logo: "https://cdn.simpleicons.org/sourcetree" },
+      { name: "Gitlab", logo: "https://cdn.simpleicons.org/gitlab" },
+      { name: "Git", logo: "https://cdn.simpleicons.org/git" },
+      { name: "Embarcadero", logo: "https://cdn.simpleicons.org/embarcadero" },
+      { name: "VS Code", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbrandlogos.net%2Fwp-content%2Fuploads%2F2021%2F11%2Fvisual-studio-code-logo.png&f=1&nofb=1&ipt=395f23f2ff4dbdf7b48f1417203c0b69231ed58a5d60b03e8567a221d2536901&ipo=images" },
     ]
   },
   {
@@ -25,22 +28,44 @@ const experiences = [
     description: "Developing LCA dashboard for assessing environmental impact of factory processes.",
     tools: [
       { name: "Java", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbrandlogos.net%2Fwp-content%2Fuploads%2F2021%2F11%2Fjava-logo.png&f=1&nofb=1&ipt=645ad72fbf1f1b9cfa1c592b1f3d877a0fc2466894bcee25e7101de1a2981551&ipo=imageshttps://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdownload.logo.wine%2Flogo%2FJava_(programming_language)%2FJava_(programming_language)-Logo.wine.png&f=1&nofb=1&ipt=7695076c9b55e3092a1005e85c0c58d4eaa6d83e854a4a86254efba8e8c8e906&ipo=images?height=48&width=48" },
-      { name: "Express", logo: "/placeholder.svg?height=24&width=24" },
-      { name: "React", logo: "/placeholder.svg?height=24&width=24" },
-      { name: "Node.js", logo: "/placeholder.svg?height=24&width=24" },
+      { name: "Haskell", logo: "https://cdn.simpleicons.org/haskell" },
+      { name: "InfluxDB", logo: "https://cdn.simpleicons.org/influxdb" },
+      { name: "Docker", logo: "https://cdn.simpleicons.org/docker" },
+      { name: "Git", logo: "https://cdn.simpleicons.org/git" },
     ]
   },
   {
-    title: "Junior Web Developer",
-    company: "WebCraft Agency",
-    logo: "/placeholder.svg?height=40&width=40",
-    period: "2015 - 2017",
-    description: "Created responsive websites and implemented CMS solutions.",
+    title: "Research Assistant",
+    company: "Aalto University School of Science and Technology",
+    logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.studyinfinland.fi%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fog_image%2Fpublic%2F2018-10%2FAalto%2520logo_0.png%3Fitok%3DBTbxbaXQ&f=1&nofb=1&ipt=03995a6c82b83b78a02c3bff036ae2161b5c9cd69e779ea2e0fb3332e85758eb&ipo=images",
+    period: "Jun 2019 - Dec 2019",
+    description: "Simulating and calculating the annihilating electron-positron pair momentum density in a solid in the atomic scale using quantum Monte Carlo methods (Master’s thesis).",
     tools: [
-      { name: "HTML5", logo: "/placeholder.svg?height=24&width=24" },
-      { name: "CSS3", logo: "/placeholder.svg?height=24&width=24" },
-      { name: "JavaScript", logo: "/placeholder.svg?height=24&width=24" },
-      { name: "WordPress", logo: "/placeholder.svg?height=24&width=24" },
+      { name: "Fortran", logo: "https://cdn.simpleicons.org/fortran" },
+      { name: "Quantum ESPRESSO", logo: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.openscience.org%2Fblog%2Fwp-content%2Fuploads%2F2009%2F01%2Fquantum_espresso6_trasparentbg.png&f=1&nofb=1&ipt=b303839d6fc58b58a54f26025a3d2da3fec77c81e00caab2cba0cc06f75f650e&ipo=images" },
+      { name: "Matlab", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdic.academic.ru%2Fpictures%2Fwiki%2Ffiles%2F77%2FMatlab_Logo.png&f=1&nofb=1&ipt=de8b9be6d426a4eeb2cfcff0b906bd53996e2b5a9945f3b004ab19acaabb3a9c&ipo=images" },
+      { name: "Github", logo: "https://cdn.simpleicons.org/github" },
+      { name: "Git", logo: "https://cdn.simpleicons.org/git" },
+      { name: "Emacs", logo: "https://cdn.simpleicons.org/gnuemacs" },
+      { name: "Overleaf", logo: "https://cdn.simpleicons.org/overleaf" },
+      { name: "LaTeX", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.clipartmax.com%2Fpng%2Fmiddle%2F271-2710063_latex-latex-logo.png&f=1&nofb=1&ipt=9204a46bdc1dc961121943d7c194207dac92eff7addf95a23c6d41aade5cba0a&ipo=images" },
+    ]
+  },
+  {
+    title: "Research Assistant",
+    company: "Aalto University School of Science and Technology",
+    logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.studyinfinland.fi%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fog_image%2Fpublic%2F2018-10%2FAalto%2520logo_0.png%3Fitok%3DBTbxbaXQ&f=1&nofb=1&ipt=03995a6c82b83b78a02c3bff036ae2161b5c9cd69e779ea2e0fb3332e85758eb&ipo=images",
+    period: "Summer 2018",
+    description: "Simulating and calculating the enhancement factor in a semiconductor in the atomic scale using quantum Monte Carlo methods (Special assignment).",
+    tools: [
+      { name: "Fortran", logo: "https://cdn.simpleicons.org/fortran" },
+      { name: "Quantum ESPRESSO", logo: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.openscience.org%2Fblog%2Fwp-content%2Fuploads%2F2009%2F01%2Fquantum_espresso6_trasparentbg.png&f=1&nofb=1&ipt=b303839d6fc58b58a54f26025a3d2da3fec77c81e00caab2cba0cc06f75f650e&ipo=images" },
+      { name: "Matlab", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdic.academic.ru%2Fpictures%2Fwiki%2Ffiles%2F77%2FMatlab_Logo.png&f=1&nofb=1&ipt=de8b9be6d426a4eeb2cfcff0b906bd53996e2b5a9945f3b004ab19acaabb3a9c&ipo=images" },
+      { name: "Github", logo: "https://cdn.simpleicons.org/github" },
+      { name: "Git", logo: "https://cdn.simpleicons.org/git" },
+      { name: "Emacs", logo: "https://cdn.simpleicons.org/gnuemacs" },
+      { name: "Overleaf", logo: "https://cdn.simpleicons.org/overleaf" },
+      { name: "LaTeX", logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.clipartmax.com%2Fpng%2Fmiddle%2F271-2710063_latex-latex-logo.png&f=1&nofb=1&ipt=9204a46bdc1dc961121943d7c194207dac92eff7addf95a23c6d41aade5cba0a&ipo=images" },
     ]
   }
 ]
@@ -69,7 +94,7 @@ export default function WorkExperience() {
         {experiences.map((exp, index) => (
           <Card key={index} className="relative">
             <div className="absolute top-4 left-4">
-              <img src={exp.logo} alt={`${exp.company} logo`} className="w-20 h-20 object-contain" />
+              <img src={exp.logo} alt={`${exp.company} logo`} className="w-24 h-24 object-contain" />
             </div>
             <CardHeader className="pt-16 pb-4">
               <CardTitle>{exp.title}</CardTitle>
