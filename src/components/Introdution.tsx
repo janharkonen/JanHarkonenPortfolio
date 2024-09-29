ccbe9f65d89c459f383a7307667ac50b2d4db7e7
Jan Härkönen <jan.harkonen@gmail.com>
2024-09-29 17:47:13 +0300

    refactor: introduction

---
+++ src/components/Introdution.tsx
@@ -0,0 +1,15 @@
+import { MapPin } from "lucide-react"
+
+export default function Introduction() {
+    return(
+        <header className="text-center space-y-4">
+          <h1 className="text-5xl font-bold">Jan Härkönen</h1>
+          <div className="flex items-center justify-center text-gray-600">
+            <MapPin className="w-4 h-4 mr-2" />
+            <span>Helsinki, Finland</span>
+            <span className="ml-2">&#127467;&#127470;</span>
+          </div>
+          <h2 className="text-2xl font-semibold text-gray-700">Full Stack Developer</h2>
+        </header>
+    )
+}
