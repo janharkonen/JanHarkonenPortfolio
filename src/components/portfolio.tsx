287f603fb3f274762aca39fb738ddf78a9056c5f
Jan Härkönen <jan.harkonen@gmail.com>
2024-10-01 20:18:26 +0300

    fix: contact button scaling

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -4,21 +4,22 @@ import PersonalProjects from './PersonalProjects'
 import ContactMe from "./ContactMe"
 
 export default function Portfolio() {
-
   return (
-    <div className="max-w-4xl mx-auto">
-      <div>
+    <>
+      <div className="top-0 left-0 right-0 z-10 bg-background">
         <ContactMe />
       </div>
-      <div className="mt-24">
-        <Introduction />
+      <div className="max-w-4xl mx-auto pt-20">
+        <div className="mt-24">
+          <Introduction />
+        </div>
+        <div className="mt-36">
+          <WorkExperience />
+        </div>
+        <div className="mt-36">
+          <PersonalProjects />
+        </div>
       </div>
-      <div className="mt-36">
-        <WorkExperience />
-      </div>
-      <div className="mt-36">
-        <PersonalProjects />
-      </div>
-    </div>
+    </>
   )
 }
