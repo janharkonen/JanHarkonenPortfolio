1fbf3348625a003c3ed15fd732d5ed4c05c2c2b2
Jan Härkönen <jan.harkonen@gmail.com>
2025-03-22 22:36:16 +0200

    create content for 1st aalto work experience

--- src/components/WorkExperienceModals/Aalto1.tsx
+++ src/components/WorkExperienceModals/Aalto1.tsx
@@ -1,6 +1,7 @@
 import { BookOpenText } from "lucide-react"
 import { Experience, handlePdfDownload } from "../WorkExperience"
 import { DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog"
+import { brandedItemBadge } from "@/data/BrandedItemsComponents.tsx"
 
 export const Aalto1 = ({ experience }: {experience: Experience}) => {
     return (
@@ -20,41 +21,72 @@ export const Aalto1 = ({ experience }: {experience: Experience}) => {
           </DialogHeader>
           <div className="grid gap-6">
             <div>
-              <h3 className="text-lg font-semibold mb-2">Technologies & Tools</h3>
-              <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4">
-                {experience.tools.map((tool: any, toolIndex: number) => (
-                    <div key={toolIndex} className="flex flex-col items-center gap-2">
-                    <img src={`${tool.logoUrl}`} alt={`${tool.name} logo`} className="w-10 h-10 object-contain" />
-                    <span className="text-sm text-center">{tool.name}</span>
-                  </div>
-                ))}
-              </div>
+              <h3 className="text-lg font-semibold mb-2">Description</h3>
+              <p className="text-base">
+                Simulating and calculating the enhancement factor in a 
+                semiconductor in the atomic scale using quantum Monte Carlo 
+                methods. I did it as a special assignment eligible for extra credits 
+                for university. Take a look (below).
+              </p>
             </div>
-            {(experience.pdfUrl || experience.pdfUrl2) && (
-                <div>
+            <div>
                 <h3 className="text-lg font-semibold mb-2">Documents</h3>
                 <div className="flex gap-4">
-                  {experience.pdfUrl && (
                     <button
-                      className="flex items-center gap-2 px-4 py-2 bg-gray-100 hover:bg-gray-200 rounded-md"
-                      onClick={(e) => handlePdfDownload(e, experience.pdfUrl)}
+                    className="flex items-center gap-2 px-4 py-2 bg-gray-100 hover:bg-gray-200 rounded-md"
+                    onClick={(e) => handlePdfDownload(e, experience.pdfUrl)}
                     >
-                      <BookOpenText className="w-5 h-5" />
-                      <span>{experience.pdfCaption}</span>
+                    <BookOpenText className="w-5 h-5" />
+                    <span>{experience.pdfCaption}</span>
                     </button>
-                  )}
-                  {experience.pdfUrl2 && (
-                      <button
-                      className="flex items-center gap-2 px-4 py-2 bg-gray-100 hover:bg-gray-200 rounded-md"
-                      onClick={(e) => handlePdfDownload(e, experience.pdfUrl2)}
-                    >
-                      <BookOpenText className="w-5 h-5" />
-                      <span>{experience.pdfCaption2}</span>
-                    </button>
-                  )}
                 </div>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold mb-2">Technologies & Tools</h3>
+                <div className="flex flex-wrap gap-4">
+                <ul className="list-disc pl-5 space-y-1 ">
+                    <li>
+                      <div className="flex items-center gap-2">
+                        <span>Programming language:</span> 
+                        {brandedItemBadge("fortran")}
+                      </div>
+                    </li>
+                    <li>
+                      <div className="flex items-center gap-2">
+                        <span>Quantum Monte Carlo simulation software:</span> 
+                        {brandedItemBadge("casino")}
+                        {brandedItemBadge("quantumespresso")}
+                      </div>
+                    </li>
+                    <li>
+                      <div className="flex items-center gap-2">
+                        <span>HPC computing:</span> 
+                        {brandedItemBadge("cscmahti")}
+                      </div>
+                    </li>
+                    <li>
+                      <div className="flex items-center gap-2">
+                        <span>Editor of choice:</span> 
+                        {brandedItemBadge("emacs")}
+                      </div>
+                    </li>
+                    <li>
+                      <div className="flex items-center gap-2">
+                        <span>Thesis:</span> 
+                        {brandedItemBadge("latex")}
+                        {brandedItemBadge("overleaf")}
+                      </div>
+                    </li>
+                    <li>
+                      <div className="flex items-center gap-2">
+                        <span>Other:</span> 
+                        {brandedItemBadge("matlab")}
+                        {brandedItemBadge("github")}
+                      </div>
+                    </li>
+                </ul>
               </div>
-            )}
+            </div>
           </div>
         </>
     )
