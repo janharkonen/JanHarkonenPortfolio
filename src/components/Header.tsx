bce5ed7caf8bf3b241c013fdc64ba17f63706373
Jan Härkönen <jan.harkonen@gmail.com>
2025-04-09 16:50:36 +0300

    padding to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import ContactMe from "@/components/ContactButtons";
 
 export default function Header() {
   return (
-    <header className="text-l sm:text-2xl sticky top-0 z-50 w-full bg-white/80 dark:bg-gray-950/80 backdrop-blur-sm shadow-md dark:shadow-gray-800">
+    <header className="p-2 sm:p-2 text-l sm:text-2xl sticky top-0 z-50 w-full bg-white/80 dark:bg-gray-950/80 backdrop-blur-sm shadow-md dark:shadow-gray-800">
       <div className="flex flex-row items-center">
         <div className="flex-1 flex justify-start text-wrap">
           Jan Härkönen
