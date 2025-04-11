66144744dfd1c06e8b8cfe81170db104b558eebe
Jan Härkönen <jan.harkonen@gmail.com>
2025-04-11 22:58:37 +0300

    fine tune portfolio site page

--- src/app/(root)/featured_projects/portfolio_site/page.tsx
+++ src/app/(root)/featured_projects/portfolio_site/page.tsx
@@ -1,8 +1,13 @@
 import { BrandedItemBadge } from "@/components/BrandedItemComponents";
+import { FeaturedProject, featuredProjects } from "@/lib/featuredProjects";
+const project: FeaturedProject = featuredProjects[3]
+
 
 export default function Home() {
   return (
-      <div className="max-w-4xl">
+    <div className="w-full">
+      <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
+      <h2 className="text-xl mb-4">2025</h2>
       <img
       src={"https://picapi.janharkonen.fi/api/pics/0cebe3a64ade44858fd946e58dd6b540.png?BGc=white&BG=101"}
       alt="Portfolio site"
@@ -32,9 +37,99 @@ export default function Home() {
               <div className="flex items-center gap-2">
                 <span>Programming language:</span> 
                 <BrandedItemBadge brandKey="typescript" />
-                <BrandedItemBadge brandKey="javascript" />
-                <BrandedItemBadge brandKey="html" />
-                <BrandedItemBadge brandKey="css" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>Framework:</span> 
+                <BrandedItemBadge brandKey="nextjs" />
+                <BrandedItemBadge brandKey="react" />
+                <BrandedItemBadge brandKey="nodejs" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>UI:</span> 
+                <BrandedItemBadge brandKey="tailwind" />
+                <BrandedItemBadge brandKey="shadcn" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>Deployment:</span> 
+                <BrandedItemBadge brandKey="vercel" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>DNS:</span> 
+                <BrandedItemBadge brandKey="cloudflare" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>AI tools:</span> 
+                <BrandedItemBadge brandKey="Claude" />
+                <BrandedItemBadge brandKey="V0" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>IDE:</span> 
+                <BrandedItemBadge brandKey="cursor" />
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-2">
+                <span>Version control</span>
+                <BrandedItemBadge brandKey="git" />
+                <BrandedItemBadge brandKey="gitkraken" />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+          
+      <div>
+        <h3 className="mt-4 text-lg font-semibold mb-2">What I learned</h3>
+        <div className="flex flex-wrap gap-4">
+          <ul className="list-disc pl-5 space-y-1">
+            <li>Next.js</li>
+          </ul>
+        </div>
+      </div>
+      
+      <div className="mt-4">
+        <h2 className="text-2xl font-bold mb-4">Older version</h2>
+        <h2 className="text-xl mb-4">2024</h2>
+        <img
+        src={"https://i4ndcxhbcg.ufs.sh/f/Qdvn5pODvKPkjGPA6wr1sfHKch8JrDNk9IYtMnRBeEzQvW0F"}
+        alt="Portfolio site old"
+        className="w-full h-auto max-h-[500px] object-contain rounded-md"
+        />
+        <h3 className="text-lg font-semibold mb-2">Overview (V1)</h3>
+        <p className="text-base">
+          This portfolio website highlights my professional journey and projects 
+          as a full-stack developer in the most appropriate fashion.
+        </p>
+      </div>
+      <div className="mt-4">
+        <h3 className="text-lg font-semibold mb-2">Key Features</h3>
+        <ul className="list-disc pl-5 space-y-1">
+            <li>Simple and clean landing page</li>
+            <li>Interactive elements</li>
+            <li>Professional experience, education and personal projects</li>
+        </ul>
+      </div>
+          
+      <div className="overflow-x-auto">
+      <h3 className="mt-4 text-lg font-semibold mb-2">Tech stack</h3>
+        <div className="flex flex-wrap gap-4">
+        <ul className="list-disc pl-5 space-y-1 ">
+            <li>
+              <div className="flex items-center gap-2">
+                <span>Programming language:</span> 
+                <BrandedItemBadge brandKey="typescript" />
               </div>
             </li>
             <li>
@@ -92,17 +187,13 @@ export default function Home() {
         <h3 className="mt-4 text-lg font-semibold mb-2">What I learned</h3>
         <div className="flex flex-wrap gap-4">
           <ul className="list-disc pl-5 space-y-1">
-            <li>Cloud VPS deployment</li>
-            <li>Containerization and Docker</li>
-            <li>Authentication and JWT</li>
-            <li>Deploying relational databases</li>
-            <li>CRON jobs</li>
-            <li>Bash scripts</li>
-            <li>Multi-tenancy</li>
+            <li>React</li>
+            <li>Web development</li>
+            <li>Web deployment</li>
+            <li>DNS</li>
           </ul>
         </div>
       </div>
-      
     </div>
   )
 }
