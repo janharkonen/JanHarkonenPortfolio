7b2dfb44ef247821333a7432ab15bfae1c456f95
Jan Härkönen <jan.harkonen@gmail.com>
2025-04-12 11:46:00 +0300

    add link to home from header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,14 @@
 
 import DarkModeToggle from "@/components/header/DarkModeToggle";
 import ContactMe from "@/components/header/ContactButtons";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="p-2 sm:p-2 text-l sm:text-2xl sticky top-0 z-50 w-full bg-white/80 dark:bg-gray-950/80 backdrop-blur-sm shadow-md dark:shadow-gray-800">
       <div className="flex flex-row items-center">
         <div className="flex-1 flex justify-start text-wrap">
-          Jan Härkönen
+          <Link href="/">Jan Härkönen</Link>
         </div>
         <div className="flex justify-center">
           <ContactMe />
