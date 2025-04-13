7c68471c8e553fb780c0853444e1c61753500073
Jan Härkönen <jan.harkonen@gmail.com>
2025-04-13 12:48:51 +0300

    improve header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,7 +9,12 @@ export default function Header() {
     <header className="p-2 sm:p-2 text-l sm:text-2xl sticky top-0 z-50 w-full bg-white/80 dark:bg-gray-950/80 backdrop-blur-sm shadow-md dark:shadow-gray-800">
       <div className="flex flex-row items-center">
         <div className="flex-1 flex justify-start text-wrap">
-          <Link href="/">Jan Härkönen</Link>
+          <Link 
+              href="/" 
+              className="text-l sm:text-xl sm:px-2 font-semibold hover:text-primary transition-colors"
+            >
+              Jan Härkönen
+            </Link>
         </div>
         <div className="flex justify-center">
           <ContactMe />
